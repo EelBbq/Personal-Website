08df8c6cb1a555f9f033241b02de5497b18e4a5a
EelBbq <ianchapman311@gmail.com>
2022-08-06 03:34:29 -0400

    added navbar, tried to figure out navbar.js+react

---
+++ navbar.js
@@ -0,0 +1,40 @@
+const root = ReactDom.createRoot(
+    document.getElementById('root')
+);
+const element = <h1>Hello, world</h1>;
+root.render(element);
+
+/*
+const navbar = (
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+);
+        
+
+const e = React.createElement;
+const domContainer = document.querySelector('#navContainer');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(NavContainer));
+*/
